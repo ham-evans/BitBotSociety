9e13050608fb04c7673774c734a746f652cef0a4
Hamilton Evans <hamevans@Hamiltons-MBP.lan>
2021-11-15 23:19:10 -0800

    site built

---
+++ src/components/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./About.css";
+import rap from '../images/bitbotRap.mp4'
+import pattern from '../images/star2.jpeg';
+
+export default function About () {
+    return (
+        <div className="about" id="project" style={{ backgroundImage: `url(${pattern})` }}>
+            <div className="about__wrapper">
+                <video controls>
+                    <source src={rap} type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+            </div>
+            <div className="about__container"  >
+                <h1>About Bit Bot Society</h1> 
+                <p>Bit Bot Society is a collection of 10,000 Robot NFTs living on the Ethereum blockchain who beep boop right into the Metaverse. Each Bit Bot NFT is uniquely generated from a pool of 152+ different traits!</p>
+            </div>
+        </div>
+    );
+}
