5bd913e3c190d9fade44772af46ba3e08b99563e
Hamilton Evans <hamevans@Hamiltons-MacBook-Pro.local>
2021-11-17 10:40:55 -0800

    video patch?

--- src/components/About.js
+++ src/components/About.js
@@ -6,12 +6,13 @@ import pattern from '../images/star2.jpeg';
 export default function About () {
     return (
         <div className="about" id="project" style={{ backgroundImage: `url(${pattern})` }}>
-            <div className="about__wrapper">
-                <video playsInline controls>
-                    <source src={rap} type="video/mp4" />
-                    Your browser does not support the video tag.
-                </video>
-            </div>
+            <div className="about__wrapper" dangerouslySetInnerHTML={{
+                __html: `<video className="app__backgroundVideo" loop playsinline controls>
+                            <source src=${rap} type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>`,
+                }}
+            />
             <div className="about__container"  >
                 <h1>About Bit Bot Society</h1> 
                 <p>Bit Bot Society is a collection of 9,999 Robot NFTs living on the Ethereum blockchain who beep boop right into the Metaverse. Each Bit Bot NFT is uniquely generated from a pool of 152+ different traits!</p>
